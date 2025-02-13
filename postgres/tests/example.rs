8e654cfa6a2f0d7474847633bc505b75adc13f3b
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2025-02-13 16:41:24 +0700

    Initial commit

---
+++ postgres/tests/example.rs
@@ -0,0 +1,15 @@
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn string_test() {
+        let s1 = "Hello";
+        let s2 = "Hello";
+        assert_eq!(s1, s2);
+    }
+}
